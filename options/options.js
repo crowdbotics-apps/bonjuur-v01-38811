33c01baa7f4e6c5b089235e8b16d5ec0cff7c7f0
omer bese <signup@bonjuur.com>
2023-02-08 15:49:08 +0300

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
   name: "bonjuur-v01-38811",
   url: "https://bonjuur-v01-38811.botics.co",
   api: "https://bonjuur-v01-38811.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
